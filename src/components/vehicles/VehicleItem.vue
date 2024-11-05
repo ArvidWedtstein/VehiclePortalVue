322c59b58336a72410acde53084306fe5bda9bb6
ArvidWedtstein <71834553+ArvidWedtstein@users.noreply.github.com>
2024-11-05 18:56:11 +0100

    updated actions

--- src/components/vehicles/VehicleItem.vue
+++ src/components/vehicles/VehicleItem.vue
@@ -14,12 +14,15 @@ defineProps<Props>();
   <div class="card bg-base-100 image-full shadow-xl">
     <figure>
       <!-- <img :src="vehicle" alt="car" loading="lazy" /> -->
+      <CarIcon v-if="vehicle.type === 'M1'" class="h-14" />
+
+      <MotorcycleIcon
+        v-if="vehicle.type === 'L3e' || vehicle.type === 'MCT'"
+        class="h-14"
+      />
     </figure>
     <div class="card-body">
-      <h2 class="card-title">{{ vehicle.make }} {{ vehicle.type }}</h2>
-      <CarIcon v-if="vehicle.type === 'M1'" />
-
-      <MotorcycleIcon v-if="vehicle.type === 'L3e' || vehicle.type === 'MCT'" class="h-15" />
+      <h2 class="card-title">{{ vehicle.make }} {{ vehicle.model }}</h2>
 
       <div class="card-actions justify-end">
         <RouterLink
